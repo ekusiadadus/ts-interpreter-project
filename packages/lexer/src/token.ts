871642582d13ba1ca8b4edf96e5736537ad53b82
ekusiadadus <ekusiadadus@gmail.com>
2024-05-15 08:21:20 +0900

    chore: rename tokentype to tokenkind

--- packages/lexer/src/token.ts
+++ packages/lexer/src/token.ts
@@ -1,61 +1,61 @@
 export enum TokenType {
-  Illegal = "ILLEGAL",
-  Eof = "EOF",
-  Ident = "IDENT",
-  Int = "INT",
-  Assign = "=",
-  Plus = "+",
-  Minus = "-",
-  Bang = "!",
-  Asterisk = "*",
-  Slash = "/",
-  Lt = "<",
-  Gt = ">",
-  Eq = "==",
-  NotEq = "!=",
-  Comma = ",",
-  Semicolon = ";",
-  LParen = "(",
-  RParen = ")",
-  LBrace = "{",
-  RBrace = "}",
-  Function = "FUNCTION",
-  Let = "LET",
-  If = "IF",
-  Else = "ELSE",
-  Return = "RETURN",
-  True = "TRUE",
-  False = "FALSE",
+  Illegal = 'ILLEGAL',
+  Eof = 'EOF',
+  Ident = 'IDENT',
+  Int = 'INT',
+  Assign = '=',
+  Plus = '+',
+  Minus = '-',
+  Bang = '!',
+  Asterisk = '*',
+  Slash = '/',
+  Lt = '<',
+  Gt = '>',
+  Eq = '==',
+  NotEq = '!=',
+  Comma = ',',
+  Semicolon = ';',
+  LParen = '(',
+  RParen = ')',
+  LBrace = '{',
+  RBrace = '}',
+  Function = 'FUNCTION',
+  Let = 'LET',
+  If = 'IF',
+  Else = 'ELSE',
+  Return = 'RETURN',
+  True = 'TRUE',
+  False = 'FALSE'
 }
 
 export class Token {
   constructor(public kind: TokenType, public literal: string) {}
 
   toString(): string {
-    return `Token(${this.kind}, "${this.literal}")`;
+    return `Token(${this.kind}, "${this.literal}")`
   }
 }
 
 // biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
-export class TokenKindUtils {
+export class TokenTypeUtils {
   static lookupIdent(identifier: string): TokenType {
     switch (identifier) {
-      case "fn":
-        return TokenType.Function;
-      case "let":
-        return TokenType.Let;
-      case "if":
-        return TokenType.If;
-      case "else":
-        return TokenType.Else;
-      case "true":
-        return TokenType.True;
-      case "false":
-        return TokenType.False;
-      case "return":
-        return TokenType.Return;
+      case 'fn':
+        return TokenType.Function
+      case 'let':
+        return TokenType.Let
+      case 'if':
+        return TokenType.If
+      case 'else':
+        return TokenType.Else
+      case 'true':
+        return TokenType.True
+      case 'false':
+        return TokenType.False
+      case 'return':
+        return TokenType.Return
       default:
-        return TokenType.Ident;
+        return TokenType.Ident
     }
   }
 }
