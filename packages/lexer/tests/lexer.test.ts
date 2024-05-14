0f3909ae5f4027fdb61edd7d06f0aead1fca2d92
ekusiadadus <ekusiadadus@gmail.com>
2024-05-14 15:06:56 +0900

    feat: init lexer wit ts

---
+++ packages/lexer/tests/lexer.test.ts
@@ -0,0 +1,29 @@
+// packages/lexer/tests/lexer.test.ts
+import { describe, it, expect } from "bun:test";
+import { Lexer } from "../src/lexer";
+import { Token, TokenType } from "../src/token";
+
+describe("Lexer", () => {
+  it("nextToken", () => {
+    const input = "=+(){},;";
+    const expected: Token[] = [
+      new Token(TokenType.Assign, "="),
+      new Token(TokenType.Plus, "+"),
+      new Token(TokenType.LParen, "("),
+      new Token(TokenType.RParen, ")"),
+      new Token(TokenType.LBrace, "{"),
+      new Token(TokenType.RBrace, "}"),
+      new Token(TokenType.Comma, ","),
+      new Token(TokenType.Semicolon, ";"),
+      new Token(TokenType.Eof, ""),
+    ];
+
+    const lexer = new Lexer(input);
+
+    expected.forEach((expectedToken, idx) => {
+      const receivedToken = lexer.nextToken();
+      expect(receivedToken.kind).toBe(expectedToken.kind);
+      expect(receivedToken.literal).toBe(expectedToken.literal);
+    });
+  });
+});
