373b7076650ca88aaaf9e3c2017d4c37504d4655
ekusiadadus <ekusiadadus@gmail.com>
2024-05-15 08:21:36 +0900

    chore: update token kint utils to token type utils

--- packages/lexer/src/lexer.ts
+++ packages/lexer/src/lexer.ts
@@ -1,149 +1,147 @@
-import { Token, TokenType } from "./token";
+import { Token, TokenTypeUtils, TokenType } from './token'
 
-// biome-ignore lint/suspicious/noExportsInTest: <explanation>
 export class Lexer {
-  private input: string[];
-  private position: number;
-  private readPosition: number;
-  private ch: string;
+  private input: string[]
+  private position: number
+  private readPosition: number
+  private ch: string
 
   constructor(input: string) {
-    this.input = input.split("");
-    this.position = 0;
-    this.readPosition = 0;
-    this.ch = "";
-    this.readChar();
+    this.input = input.split('')
+    this.position = 0
+    this.readPosition = 0
+    this.ch = ''
+    this.readChar()
   }
 
   private readChar() {
     if (this.readPosition >= this.input.length) {
-      this.ch = "\0";
+      this.ch = '\0'
     } else {
-      this.ch = this.input[this.readPosition];
+      this.ch = this.input[this.readPosition]
     }
 
-    this.position = this.readPosition;
-    this.readPosition += 1;
+    this.position = this.readPosition
+    this.readPosition += 1
   }
 
   public nextToken(): Token {
-    this.skipWhitespace();
+    this.skipWhitespace()
 
-    let token: Token;
+    let token: Token
     switch (this.ch) {
-      case "=":
-        if (this.peekChar() === "=") {
-          const ch = this.ch;
-          this.readChar();
-          token = new Token(TokenType.Eq, ch + this.ch);
+      case '=':
+        if (this.peekChar() === '=') {
+          const ch = this.ch
+          this.readChar()
+          token = new Token(TokenType.Eq, ch + this.ch)
         } else {
-          token = new Token(TokenType.Assign, this.ch);
+          token = new Token(TokenType.Assign, this.ch)
         }
-        break;
-      case ";":
-        token = new Token(TokenType.Semicolon, this.ch);
-        break;
-      case "(":
-        token = new Token(TokenType.LParen, this.ch);
-        break;
-      case ")":
-        token = new Token(TokenType.RParen, this.ch);
-        break;
-      case ",":
-        token = new Token(TokenType.Comma, this.ch);
-        break;
-      case "+":
-        token = new Token(TokenType.Plus, this.ch);
-        break;
-      case "-":
-        token = new Token(TokenType.Minus, this.ch);
-        break;
-      case "!":
-        if (this.peekChar() === "=") {
-          const ch = this.ch;
-          this.readChar();
-          token = new Token(TokenType.NotEq, ch + this.ch);
+        break
+      case ';':
+        token = new Token(TokenType.Semicolon, this.ch)
+        break
+      case '(':
+        token = new Token(TokenType.LParen, this.ch)
+        break
+      case ')':
+        token = new Token(TokenType.RParen, this.ch)
+        break
+      case ',':
+        token = new Token(TokenType.Comma, this.ch)
+        break
+      case '+':
+        token = new Token(TokenType.Plus, this.ch)
+        break
+      case '-':
+        token = new Token(TokenType.Minus, this.ch)
+        break
+      case '!':
+        if (this.peekChar() === '=') {
+          const ch = this.ch
+          this.readChar()
+          token = new Token(TokenType.NotEq, ch + this.ch)
         } else {
-          token = new Token(TokenType.Bang, this.ch);
+          token = new Token(TokenType.Bang, this.ch)
         }
-        break;
-      case "*":
-        token = new Token(TokenType.Asterisk, this.ch);
-        break;
-      case "/":
-        token = new Token(TokenType.Slash, this.ch);
-        break;
-      case "<":
-        token = new Token(TokenType.Lt, this.ch);
-        break;
-      case ">":
-        token = new Token(TokenType.Gt, this.ch);
-        break;
-      case "{":
-        token = new Token(TokenType.LBrace, this.ch);
-        break;
-      case "}":
-        token = new Token(TokenType.RBrace, this.ch);
-        break;
-      case "\0":
-        token = new Token(TokenType.Eof, "");
-        break;
+        break
+      case '*':
+        token = new Token(TokenType.Asterisk, this.ch)
+        break
+      case '/':
+        token = new Token(TokenType.Slash, this.ch)
+        break
+      case '<':
+        token = new Token(TokenType.Lt, this.ch)
+        break
+      case '>':
+        token = new Token(TokenType.Gt, this.ch)
+        break
+      case '{':
+        token = new Token(TokenType.LBrace, this.ch)
+        break
+      case '}':
+        token = new Token(TokenType.RBrace, this.ch)
+        break
+      case '\0':
+        token = new Token(TokenType.Eof, '')
+        break
       default:
         if (this.isLetter(this.ch)) {
-          const literal = this.readIdentifier();
-          return new Token(TokenType.lookupIdent(literal), literal);
-        } else if (this.isDigit(this.ch)) {
-          const literal = this.readNumber();
-          return new Token(TokenType.Int, literal);
-        } else {
-          token = new Token(TokenType.Illegal, this.ch);
+          const literal = this.readIdentifier()
+          return new Token(TokenTypeUtils.lookupIdent(literal), literal)
         }
+        if (this.isDigit(this.ch)) {
+          const literal = this.readNumber()
+          return new Token(TokenType.Int, literal)
+        }
+        token = new Token(TokenType.Illegal, this.ch)
     }
 
-    this.readChar();
-    return token;
+    this.readChar()
+    return token
   }
 
   private isLetter(ch: string): boolean {
-    return (ch >= "a" && ch <= "z") || (ch >= "A" && ch <= "Z") || ch === "_";
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch === '_'
   }
 
   private isDigit(ch: string): boolean {
-    return ch >= "0" && ch <= "9";
+    return ch >= '0' && ch <= '9'
   }
 
   private readNumber(): string {
-    const position = this.position;
+    const position = this.position
     while (this.isDigit(this.ch)) {
-      this.readChar();
+      this.readChar()
     }
-    return this.input.slice(position, this.position).join("");
+    return this.input.slice(position, this.position).join('')
   }
 
   private readIdentifier(): string {
-    const position = this.position;
+    const position = this.position
     while (this.isLetter(this.ch)) {
-      this.readChar();
+      this.readChar()
     }
-    return this.input.slice(position, this.position).join("");
+    return this.input.slice(position, this.position).join('')
   }
 
   private peekChar(): string {
     if (this.readPosition >= this.input.length) {
-      return "\0";
-    } else {
-      return this.input[this.readPosition];
+      return '\0'
     }
+    return this.input[this.readPosition]
   }
 
   private skipWhitespace() {
     while (
-      this.ch === " " ||
-      this.ch === "\t" ||
-      this.ch === "\n" ||
-      this.ch === "\r"
+      this.ch === ' ' ||
+      this.ch === '\t' ||
+      this.ch === '\n' ||
+      this.ch === '\r'
     ) {
-      this.readChar();
+      this.readChar()
     }
   }
 }
